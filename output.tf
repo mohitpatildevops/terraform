a64f9c2161a79260875833ef38dc8a8adc855e02
Mohit Patil <121102920+mohitpatildevops@users.noreply.github.com>
2026-03-02 15:24:32 +0530

    Update output

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.mohit.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.mohit.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.mohit_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.mohit_subnet[*].id
 }
 
